043c85d766bc5ea9f0a3f8b69345a0c5991bf285
Nikita Zeleniak <zeleniak.nikita@gmail.com>
2025-03-22 16:17:41 -0400

    done

--- src/repository/contacts_book.py
+++ src/repository/contacts_book.py
@@ -4,7 +4,7 @@ from typing import Sequence
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.entity.models import Contact_Book
+from src.entity.models import Contact_Book, User
 from src.schemas.contact_book import ContactBookSchema, ContactBookUpdateSchema, ContactBookResponse
 
 logger = logging.getLogger("uvicorn.error")
@@ -14,34 +14,34 @@ class ContactBookRepository:
     def __init__(self, session: AsyncSession):
         self.db = session
 
-    async def get_contact(self, limit: int, offset: int) -> Sequence[Contact_Book]:
-        stmt = select(Contact_Book).offset(offset).limit(limit)
+    async def get_contact(self, limit: int, offset: int, user: User) -> Sequence[Contact_Book]:
+        stmt = select(Contact_Book).filter_by(user_id = user.id).offset(offset).limit(limit)
         contact = await self.db.execute(stmt)
         return contact.scalars().all()
 
-    async def get_contact_by_id(self, contact_id: int) -> Contact_Book | None:
-        stmt = select(Contact_Book).filter_by(id=contact_id)
+    async def get_contact_by_id(self, contact_id: int, user: User) -> Contact_Book | None:
+        stmt = select(Contact_Book).filter_by(id=contact_id, user_id = user.id)
         contact = await self.db.execute(stmt)
         return contact.scalar_one_or_none()
 
-    async def create_contact(self, body: ContactBookSchema) -> Contact_Book:
-        contact = Contact_Book(**body.model_dump())
+    async def create_contact(self, body: ContactBookSchema, user: User) -> Contact_Book:
+        contact = Contact_Book(**body.model_dump(), user=user)
         self.db.add(contact)
         await self.db.commit()
         await self.db.refresh(contact)
         return contact
 
-    async def remove_contact(self, contact_id: int) -> Contact_Book | None:
-        contact = await self.get_contact_by_id(contact_id)
+    async def remove_contact(self, contact_id: int ,user: User) -> Contact_Book | None:
+        contact = await self.get_contact_by_id(contact_id, user)
         if contact:
             await self.db.delete(contact)
             await self.db.commit()
         return contact
 
     async def update_contact(
-            self, contact_id: int, body: ContactBookUpdateSchema
+            self, contact_id: int, body: ContactBookUpdateSchema,user: User
     ) -> Contact_Book:
-        contact = await self.get_contact_by_id(contact_id)
+        contact = await self.get_contact_by_id(contact_id, user)
         if contact:
             update_data = body.model_dump(exclude_unset=True)
 
